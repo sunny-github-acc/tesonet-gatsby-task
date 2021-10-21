3997ec96becf6cbbfa097fdd43a84ea1df936f51
sunny-github-acc <karolis.kazak@gmail.com>
2021-10-21 11:25:31 +0300

    Updates

---
+++ component---src-pages-404-tsx-f28bd37a32c3956364c4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktesonet_gatsby_task=self.webpackChunktesonet_gatsby_task||[]).push([[218],{938:function(e,t,n){n.r(t);var s=n(7294);t.default=function(){return s.createElement("main",null,s.createElement("h1",null,"woops.."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-f28bd37a32c3956364c4.js.map
